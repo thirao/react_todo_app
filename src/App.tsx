53b855989386db119356ebc85b124a9494b04e4c
t-hirao <hirao.tatsuya@gmail.com>
2019-10-21 20:37:51 +0900

    add material-ui list

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SimpleList from './components/hoge'
 
 const App: React.FC = () => {
   return (
@@ -8,17 +9,14 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          sample Todo List
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body 
+                className="List-block"
+      >
+        <SimpleList />
+      </body>
     </div>
   );
 }
